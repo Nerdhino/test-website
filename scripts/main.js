063fdde5b79dd566ff6bae8d42c0a0ba374b7dea
Nerdhino <alexander.arndt2010@gmail.com>
2018-07-01 06:51:34 +0200

    Test

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Starting Somewhere!';
+
+var myVariable = 'Bob';
+myvariable = 'Steve';
+
+/*
+Everything in between is a comment.
+*/
+
+// This is also a comment.
